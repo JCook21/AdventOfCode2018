eff92082a3fbe13ac2c1ef67ba78ff02eb1f7315
Jeremy Cook <jeremycook0@icloud.com>
2018-12-05 12:58:34 -0500

    Minor code cleanup

--- 3/overlaps.rb
+++ 3/overlaps.rb
@@ -4,7 +4,7 @@ require 'set'
 class FabricSquare
   alias == eql?
   attr_accessor :id
-  @regexp = /(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/
+
   def initialize(x, y, id)
     @x = x
     @y = y
@@ -20,13 +20,17 @@ class FabricSquare
   end
 
   def self.parse_from_string(raw)
-    matcher = @regexp.match(raw)
+    matcher = /(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/.match(raw)
     id = matcher[1]
     x_start = matcher[2].to_i
     y_start = matcher[3].to_i
     width = matcher[4].to_i
     height = matcher[5].to_i
-    (0...width).flat_map { |x| (0...height).map { |y| FabricSquare.new(x + x_start, y + y_start, id) } }
+    (0...width).flat_map do |x|
+      (0...height).map do |y|
+        FabricSquare.new(x + x_start, y + y_start, id)
+      end
+    end
   end
 
   protected
