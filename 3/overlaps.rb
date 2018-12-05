d21c84dd17b4d38175d42cf7a7e97d5f3fa3077a
Jeremy Cook <jeremycook0@icloud.com>
2018-12-05 14:52:02 -0500

    Small cleanup to static methods

--- 3/overlaps.rb
+++ 3/overlaps.rb
@@ -3,6 +3,29 @@ require 'set'
 
 # Value object representing a fabric square in advent of code day 3
 class FabricSquare
+  class << FabricSquare
+    def parse_from_string(raw)
+      id, x_start, y_start, width, height = parse_raw(raw)
+      (0...width).flat_map do |x|
+        (0...height).map do |y|
+          FabricSquare.new(x + x_start, y + y_start, id)
+        end
+      end
+    end
+
+    protected
+
+    def parse_raw(raw)
+      matcher = /(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/.match(raw)
+      id = matcher[1]
+      x_start = matcher[2].to_i
+      y_start = matcher[3].to_i
+      width = matcher[4].to_i
+      height = matcher[5].to_i
+      [id, x_start, y_start, width, height]
+    end
+  end
+
   alias == eql?
   attr_accessor :id
 
@@ -18,20 +41,6 @@ class FabricSquare
 
   def hash
     state.hash
-  end
-
-  def self.parse_from_string(raw)
-    matcher = /(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/.match(raw)
-    id = matcher[1]
-    x_start = matcher[2].to_i
-    y_start = matcher[3].to_i
-    width = matcher[4].to_i
-    height = matcher[5].to_i
-    (0...width).flat_map do |x|
-      (0...height).map do |y|
-        FabricSquare.new(x + x_start, y + y_start, id)
-      end
-    end
   end
 
   protected
